9d60b4a696dd1874cfcf63d7aab594fc77989ae9
NelakaWith <nelaka.with@gmail.com>
2025-11-20 20:11:59 +0530

    chore(ui): remove package.json and tsconfig.json files

--- packages/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./components/button";
-export * from "./components/dialog";
-export * from "./components/dropdown-menu";
-export * from "./components/popover";
-export * from "./components/tooltip";
