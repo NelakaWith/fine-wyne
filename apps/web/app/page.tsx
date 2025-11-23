62b6021ac876fd04491cbb536e43f1a7bbf88380
NelakaWith <nelaka.with@gmail.com>
2025-11-23 17:18:08 +0530

    feat(home): remove Button component from Home page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@radix-ui/themes";
 export default function Home() {
   return (
     <main>
-      <div>
-        <Button>Let&apos;s go</Button>
-      </div>
+      <div></div>
     </main>
   );
 }
